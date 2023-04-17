fdfba30ea8f1650ce7a93bc3c6cb32413bf17e9c
WONI2 <126743165+WONI2@users.noreply.github.com>
2023-04-17 19:02:11 +0900

    [Bronze V] Title: ??!, Time: 216 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String signupId =sc.nextLine();
+        String id = "??!";
+        String result = signupId+id;
+
+        System.out.println(result);
+        sc.close();
+    }
+}
