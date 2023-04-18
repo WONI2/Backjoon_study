422d3b8b4d1a7331a2468e195fc527852736224a
WONI2 <126743165+WONI2@users.noreply.github.com>
2023-04-18 21:10:09 +0900

    [Bronze V] Title: 합, Time: 208 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,25 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int t = sc.nextInt();
+		int sum=0;
+		for(int i =1; i<= t; i++ ) {
+				sum = sum+i; 
+			
+		}
+		
+		
+		System.out.println(sum);
+		
+		
+		
+	}
+
+}
